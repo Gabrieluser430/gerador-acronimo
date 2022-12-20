c1bb047fdb813c5a8cfdb823c207c8f22bd374ae
Gabriel O <107634249+Gabrieluser430@users.noreply.github.com>
2022-12-19 23:28:22 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿try
+{
+    // Input do usuário
+    Console.WriteLine("Please enter a phrase to convert to an acronym: ");
+    String input = Console.ReadLine();
+
+    // Exclusões opcionais
+    String exclusions = input.Replace("and", "");
+    String[] wordArr = exclusions.Split(" ");
+    Console.WriteLine(wordArr.Length);
+
+    // Loop
+    String acronym = "";
+    for (int i = 0; i < wordArr.Length; i++ ) 
+    {
+        if (wordArr[i] != "")
+        {
+            acronym += wordArr[i][0];
+        }
+        acronym = acronym.ToUpper();
+
+        Console.WriteLine($"Your acronym is: {acronym}");
+    }
+}
+catch(Exception e)
+{
+    Console.WriteLine(e.ToString());
+}
